7021f260a3fb97105b5b8f280766422aa717dd0b
Taha Bouhsine <45195299+skywolfmo@users.noreply.github.com>
2024-03-13 00:47:02 +0000

    removed convnext

--- src/utils/models/build_model.py
+++ src/utils/models/build_model.py
@@ -9,12 +9,12 @@ def build_model(config):
     inputs = Input(shape=(config.img_height, config.img_width, num_channels))
     
     # Define base models for each modality
-    # base_model_rgb = CNN(input_shape=(config.img_height, config.img_width, num_channels))
-    base_model_rgb = ConvNeXtTiny(
-        model_name="convnext_small",
-        include_top=False,
-        weights="imagenet",
-    )
+    base_model_rgb = CNN(input_shape=(config.img_height, config.img_width, num_channels))
+    # base_model_rgb = ConvNeXtTiny(
+    #     model_name="convnext_small",
+    #     include_top=False,
+    #     weights="imagenet",
+    # )
     base_model_rgb.trainable = config.trainable_epochs == 0
     x_rgb = base_model_rgb(inputs)
     x_rgb = GlobalAveragePooling2D()(x_rgb)

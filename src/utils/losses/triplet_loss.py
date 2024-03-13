0d30a607a4577db0dd189750a276e4f4dcd10a46
Taha Bouhsine <45195299+skywolfmo@users.noreply.github.com>
2024-03-13 00:50:07 +0000

    replaced sum by reduce_sum
    ;

--- src/utils/losses/triplet_loss.py
+++ src/utils/losses/triplet_loss.py
@@ -6,8 +6,8 @@ def loss_function(y_pred, margin=1.0):
     This nested function calculates the loss for a given batch.
     """
     anchor, positive, negative = tf.split(y_pred, 3, axis=0)
-    ap_distance = tf.sum(tf.square(anchor - positive), -1)
-    an_distance = tf.sum(tf.square(anchor - negative), -1)
+    ap_distance = tf.reduce_sum(tf.square(anchor - positive), -1)
+    an_distance = tf.reduce_sum(tf.square(anchor - negative), -1)
 
     losses = tf.maximum(0.0, ap_distance - an_distance + margin)
     return tf.reduce_mean(losses)
